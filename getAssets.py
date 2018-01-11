72e554f2eb94e8592fc8c0eaefd8a305f2775131
Mikhail Ameerally <32855759+MikhailAmeerally@users.noreply.github.com>
2018-01-10 22:50:26 -0500

    Added script to download assets

---
+++ getAssets.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+'''
+__author__ = 'Divyansh Prakash'
+__edited__ = 'Mikhail Ameerally'
+'''
+
+import sys
+import subprocess
+
+if __name__ == '__main__':
+    github_src = 'https://github.com/MikhailAmeerally/War/tree/master/War/Assets.xcassets'
+
+    try:
+      head, branch_etc = github_src.split('/tree/')
+      folder_url = '/'.join(branch_etc.split('/')[1:])
+    except:
+      print 'err:\tnot a valid folder url!'
+    else:
+      print 'fetching...'
+      subprocess.call(['svn', 'checkout', '/'.join([head, 'trunk', folder_url])])
+  else:
+    print 'use:\tgitget.py https://github.com/user/project/tree/branch-name/folder\n'
+
